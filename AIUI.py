d0250c1c7eb398bf1546721acbfe1f1fefe56c2d
Yuan Chen <67477126+andy1213812@users.noreply.github.com>
2022-03-10 09:47:03 -0500

    upload

--- AIUI.py
+++ AIUI.py
@@ -3,34 +3,35 @@ import os
 import tkinter.font as font
 import main
 
+
 def start():
     main.start()
 
-root = Tk()
-root.geometry('600x320')
-root.resizable(False,False)
-root.title("Blind Assist Tool")
-  
-path1 = os.path.abspath(__file__)
-path = os.path.dirname(path1)
+    root = Tk()
+    root.geometry('600x320')
+    root.resizable(False, False)
+    root.title("Blind Assist Tool")
 
-canvas = Canvas(width=600, height=320, highlightthickness=0, borderwidth=0)
-canvas.place(x=0, y=0)
-Imagepath = str(path) + '\\Images\\background_600.png'
+    path1 = os.path.abspath(__file__)
+    path = os.path.dirname(path1)
 
-bg = PhotoImage(file=Imagepath)
-bgid = canvas.create_image(0, 0, image=bg, anchor='nw')
+    canvas = Canvas(width=600, height=320, highlightthickness=0, borderwidth=0)
+    canvas.place(x=0, y=0)
+    Imagepath = str(path) + '\\Images\\background_600.png'
 
-ft = font.Font(family = 'Microsoft yahei', size = 15, weight=font.BOLD)
-label2 = Label(root, text = "Welcome", bg = "#FCC3A5",fg = "#3D5D72", font = ft)
-label2.pack(pady = 50)
+    bg = PhotoImage(file=Imagepath)
+    bgid = canvas.create_image(0, 0, image=bg, anchor='nw')
 
-frame1 = Frame(root)
-frame1.pack(pady = 20 )
+    ft = font.Font(family='Microsoft yahei', size=15, weight=font.BOLD)
+    label2 = Label(root, text="Welcome", bg="#FCC3A5", fg="#3D5D72", font=ft)
+    label2.pack(pady=50)
 
-ft2 = font.Font(family = 'Microsoft yahei', size = 17)
-button1 = Button(root, text = 'Voice Over', bg='#F7DFDF',command=start, font = ft2)
-button1.place(x=110, y=135, width=380, height=140)
+    frame1 = Frame(root)
+    frame1.pack(pady=20)
 
-root.mainloop()
+    ft2 = font.Font(family='Microsoft yahei', size=17)
+    button1 = Button(root, text='Voice Over',
+                     bg='#F7DFDF', command=start, font=ft2)
+    button1.place(x=110, y=135, width=380, height=140)
 
+    root.mainloop()
