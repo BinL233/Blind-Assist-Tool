881f715d145c2631dbed364f3d1c6adfd9736bd6
Yuan Chen <67477126+andy1213812@users.noreply.github.com>
2022-03-10 10:22:07 -0500

    update Pranay's AI

---
+++ AI.py
@@ -0,0 +1,18 @@
+import json
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+
+
+credential = json.load(open('credential.json'))
+API_KEY = credential['API_KEY']
+ENDPOINT = credential['ENDPOINT']
+cv_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(API_KEY))
+
+img_url1 = 'https://m.media-amazon.com/images/I/619BkvKW35L._SL1500_.jpg'
+
+
+description = cv_client.describe_image(img_url1)
+
+
+for caption in description.captions:
+    print(f"Image Description: {caption.text}")
---
+++ ImageAI.py
@@ -0,0 +1,4 @@
+{
+  "API_KEY": "a59c0f898b214d6f85f10e502de2de18",
+  "ENDPOINT": "https://computer-vision-prototype-ai.cognitiveservices.azure.com/"
+}
